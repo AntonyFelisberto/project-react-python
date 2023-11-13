795056ebc06f7d0b4f7441defa6902c423a6374c
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-11-13 20:27:27 -0300

    adicionando list

--- projeto/frontend/src/ListComponent.js
+++ projeto/frontend/src/ListComponent.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import ItemComponent from './ItemComponent'
 
 export default function ListComponent(){
     return (
         <div>
             <h2>Minha Lista</h2>
             <ul>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
+                <ItemComponent name={"meu item"}/>
                 <li>Item 1</li>
                 <li>Item 2</li>
             </ul>
---
+++ projeto/frontend/src/UserList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListComponent from './ListComponent'
+
+export default function UserList(){
+    return (
+        <div>
+            <ListComponent listName={"minha lista"}/>
+        </div>
+    )
+}
